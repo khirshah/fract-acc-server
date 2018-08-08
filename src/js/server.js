c01866748e50c45c846c63686b257fff5b4a14bd
khirshah <khirshah@gmail.com>
2018-08-08 18:57:47 +0100

    write to db function implemented

--- src/js/server.js
+++ src/js/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 var path = require('path')
 const app = express()
 const asyncHandler = require('express-async-handler')
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var database = require('./mongo');
 var dataRow = require('./dataSchema');
@@ -14,9 +17,9 @@ res.setHeader('Access-Control-Allow-Credentials', true);
 next();
 });
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/mongo', asyncHandler(async (req, res, next) => {
-	//console.log("req arrived: ", req);
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/mongoRead', asyncHandler(async (req, res, next) => {
+
 	const f = await dataRow.find({}, function(err, users) {
 	  	if (err) throw err;
 
@@ -28,22 +31,17 @@ app.get('/mongo', asyncHandler(async (req, res, next) => {
 	res.send(f)
 }))
 
-var row = new dataRow({
-    "USD": 7800,
-    "GBP_PROJ": 56,
-    "GBP": 0.00,
-    "TRANS_TYPE": "IC",
-    "TRANS_DATE": "2017-07-12",
-    "TRANS_REF": "#CS24",
-    "TRANS_DESC": "$7800.00 USD  =  £6037.27 GBP  [inv: £6064.94 | 31/05/17]",
-    "XCH_USD_GBP": 0.7740096914,
-    "XCH_GBP_USD": 1.2919734870
-});
+app.post('/mongoWrite', asyncHandler(async (req, res, next) => {
+	
+	var row = new dataRow(req.body)
 
-row.save(function(err) {
-  if (err) throw err;
+	f = await row.save(function(err) {
+	  if (err) throw err;
 
-  console.log('Data saved successfully!');
-});
+	  console.log('Data saved successfully!');
+	});
+	res.send()
+}))
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
